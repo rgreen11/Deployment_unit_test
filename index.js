56ef03c5c1e50b6cd34980154ca47e432040506d
Richard Green <richardgreen@pursuit.org>
2019-03-20 20:59:46 -0400

    testing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/ping', (req, res)=>{
     res.json({'pong': 'true'})
 })
 
-const port = 5000
-app.listen(process.env.PORT || port, ()=>{
+const port = process.env.PORT || 5000
+app.listen(port, ()=>{
     console.log('lisenting on port 5000')
 })
